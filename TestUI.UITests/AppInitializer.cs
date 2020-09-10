a9c0297249e2ece001a7480d72af1706f80c8de5
Vegard Aaberge <vegard.aaberge@gmail.com>
2020-09-10 14:20:20 +0800

    Improve the tests so that it does not rely on coordinates.

--- TestUI.UITests/AppInitializer.cs
+++ TestUI.UITests/AppInitializer.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using Xamarin.UITest;
+using Xamarin.UITest.Configuration;
 using Xamarin.UITest.Queries;
 
 namespace TestUI.UITests
 {
     public class AppInitializer
     {
-        public static IApp StartApp(Platform platform)
+        public static IApp StartApp(Platform platform, bool clearData)
         {
             if (platform == Platform.Android)
             {
-                return ConfigureApp.Android.EnableLocalScreenshots().StartApp();
+                return ConfigureApp.Android.EnableLocalScreenshots().StartApp(clearData ? AppDataMode.Clear : AppDataMode.DoNotClear);
             }
 
-            return ConfigureApp.iOS.EnableLocalScreenshots().StartApp();
+            return ConfigureApp.iOS.EnableLocalScreenshots().StartApp(clearData ? AppDataMode.Clear : AppDataMode.DoNotClear);
         }
     }
 }
--- TestUI.UITests/Tests.cs
+++ TestUI.UITests/Tests.cs
@@ -24,41 +24,64 @@ namespace TestUI.UITests
         [SetUp]
         public void BeforeEachTest()
         {
-            app = AppInitializer.StartApp(platform);
+            app = AppInitializer.StartApp(platform, true);
         }
 
         [Test]
         public void CanNavigate()
         {
+            // Wait for welcome screen
             AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Trips!"));
             app.Screenshot("Welcome screen.");
             Assert.IsTrue(results.Any());
 
+            // Tap the trips button
             app.Tap(AutomationId.TripsButton);
             results = app.WaitForElement(c => c.Marked(AutomationId.AddNewButton));
             Assert.IsTrue(results.Any());
 
+            // Tap the add new button
             app.Tap(c => c.Marked(AutomationId.AddNewButton));
             results = app.WaitForElement(c => c.Marked(AutomationId.EntryNotes));
             Assert.IsTrue(results.Any());
 
+            // Fill in the fields
             app.EnterText(c => c.Marked(AutomationId.EntryTitle), "Title");
             app.ClearText(c => c.Marked(AutomationId.EntryRating));
             app.EnterText(c => c.Marked(AutomationId.EntryRating), "5");
             app.EnterText(c => c.Marked(AutomationId.EntryNotes), "Here are some notes");
-            app.Tap(c => c.Marked(AutomationId.AddDetailsSaveButton));
 
-            Thread.Sleep(1000);
-            app.TapCoordinates(300, 200);
-            app.TapCoordinates(300, 250);
-            app.TapCoordinates(300, 300);
+            // Return from the AddDetailsPage
+            app.Tap(c => c.Marked(AutomationId.AddDetailsSaveButton));
+            results = app.WaitForElement(c => c.Marked(AutomationId.TripsListView));
+            Assert.IsTrue(results.Any());
+
+            // Tap the first element
+            var length = app.Query(c => c.Marked(AutomationId.TripsListView).Child()).Length;
+            Assert.IsTrue(length == 1);
+            var item = app.Query(c => c.Marked(AutomationId.TripsListView).Index(0));
+            app.Tap(c => c.Text("Title"));
             results = app.WaitForElement(c => c.Marked(AutomationId.TripDetailsStack));
             Assert.IsTrue(results.Any());
 
+            // Return from the details page
             Thread.Sleep(1000);
             app.Back();
             results = app.WaitForElement(c => c.Marked(AutomationId.TripsListView));
             Assert.IsTrue(results.Any());
+        }
+
+        Func<AppQuery, AppQuery> GetElementFromList(string automationId, int index)
+        {
+            Func<AppQuery, AppQuery> itemInListView = null;
+
+            if (platform == Platform.Android)
+                itemInListView = x => x.Class("ViewCellRenderer_ViewCellContainer").Index(index);
+            else if (platform == Platform.iOS)
+                itemInListView =
+                x => x.Marked(automationId).Index(0);
+
+            return itemInListView;
         }
     }
 }
